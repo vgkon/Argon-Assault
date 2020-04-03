d6350f0211e9946cc3d5d7e109286f0eb3d7023c
Roxas-PC\Roxas <vgkonstantakopoulos@gmail.com>
2020-04-04 02:12:47 +0300

    Camera, Waypoints, Ship Movement X/Y Axis

---
+++ Assets/Game.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In m/s^-1")][SerializeField] float xSpeed = 35f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TranslateX();
+        TranslateY();
+    }
+
+    private void TranslateY()
+    {
+        float yThrow = Input.GetAxis("Vertical");
+        float yOffsetThisFrame = yThrow * xSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffsetThisFrame;
+        float clampedYPos = Mathf.Clamp(rawYPos, -17f, 17f);
+
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
+    }
+
+    private void TranslateX()
+    {
+        float xThrow = Input.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffsetThisFrame;
+        float clampedXPos = Mathf.Clamp(rawXPos, -22f, 22f);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
